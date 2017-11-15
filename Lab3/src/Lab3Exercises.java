f4a6df907cb2a06f0bd17b7e362aecc86bfdc69f
diegovelez <diegovelez@192.168.0.4>
2017-11-15 18:40:52 -0400

    Creacion de la clas MyPanelClass

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,6 +3,10 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Diego Velez");
+		
+		 MyPanelClass myPanel = new MyPanelClass();
+         myFrame.getContentPane().add(myPanel);
+		
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(600, 250);
 		myFrame.setSize(200, 200);
